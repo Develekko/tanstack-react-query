f3603177876d99f6f05eb0a711a7539e13a90fb2
kimz code <81085401+KimzCodes@users.noreply.github.com>
2025-01-25 02:25:58 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Container,
+  Nav,
+  Navbar,
+  Table,
+  Row,
+  Col,
+  Form,
+  ButtonGroup,
+  Button,
+} from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">React-Query</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#home">New Post</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Row className="mt-5">
+        <Col xs={9}>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Status</th>
+                <th style={{ width: "10%" }}>Top Rate</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>lurem ipsum dollar asd sad asdas dsa dsa dsad asdasd</td>
+                <td>Otto</td>
+                <td style={{ textAlign: "center" }}>
+                  <Form.Check type="checkbox" />
+                </td>
+                <td>
+                  <ButtonGroup aria-label="Basic example">
+                    <Button variant="success">Edit</Button>
+                    <Button variant="danger">Delete</Button>
+                  </ButtonGroup>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+        <Col>
+          <h5>Filter By Status</h5>
+          <Form.Select>
+            <option value="">Select Status</option>
+            <option value="Publish">Publish</option>
+            <option value="Draft">Draft</option>
+            <option value="Blocked">Blocked</option>
+          </Form.Select>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default App
+export default App;
