bf0ed977ef0adab94bfaf6198ea307c1c60d8825
kimz code <81085401+KimzCodes@users.noreply.github.com>
2025-01-25 06:38:21 +0200

    finish

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Main from "./layouts/Main";
@@ -39,5 +41,6 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    <ReactQueryDevtools />
   </QueryClientProvider>
 );
