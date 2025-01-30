3be01780e8852ee593ac8830a76ab500f1fc34fb
kimz code <81085401+KimzCodes@users.noreply.github.com>
2025-01-30 16:39:15 +0200

    finish

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Main from "./layouts/Main";
 import Home from "./pages/Home";
 import Create from "./pages/Create";
-import Edit from "./pages/Edit";
 import Info from "./pages/Info";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -23,10 +24,6 @@ const router = createBrowserRouter([
         element: <Create />,
       },
       {
-        path: "/edit",
-        element: <Edit />,
-      },
-      {
         path: "/info",
         element: <Info />,
       },
@@ -35,5 +32,7 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
 );
