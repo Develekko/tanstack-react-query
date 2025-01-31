dc79a55439c42a49918725ff34920c50da8705ed
kimz code <81085401+KimzCodes@users.noreply.github.com>
2025-01-31 16:54:19 +0200

    finish

--- src/hooks/useGetPosts.ts
+++ src/hooks/useGetPosts.ts
@@ -1,12 +1,20 @@
 import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+
+interface PostItem {
+  id: number;
+  title: string;
+  body: string;
+  status: "published" | "draft" | "block";
+  topRate: boolean;
+}
 
 const fetchPosts = async () => {
   const result = await axios.get("http://localhost:5005/posts");
   return result.data;
 };
 
-const useGetPosts = () => {
+const useGetPosts = (): UseQueryResult<PostItem[]> => {
   return useQuery({ queryKey: ["posts"], queryFn: fetchPosts });
 };
 
